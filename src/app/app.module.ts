15c2e5c98a61eb6a98f21621bcd42ba43775a7c5
sramirez <sramirez@solidgeargroup.com>
2018-03-20 20:48:09 +0100

    Added comm with firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,26 +8,41 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import {NewGamePage} from "../pages/new-game/new-game";
 
+// Import the Firebase Module
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCLpIWG1rWhh18Y06vf1C3-Ol0IVdZ04Ws",
+    authDomain: "ionic-videogames-li.firebaseapp.com",
+    databaseURL: "https://ionic-videogames-li.firebaseio.com",
+    projectId: "ionic-videogames-li",
+    storageBucket: "ionic-videogames-li.appspot.com",
+    messagingSenderId: "1068483452051"
+}
+
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-      NewGamePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-      NewGamePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage,
+        NewGamePage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp),
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFireDatabaseModule
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        NewGamePage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
 })
 export class AppModule {}
--- src/pages/new-game/new-game.ts
+++ src/pages/new-game/new-game.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {AngularFireDatabase} from "angularfire2/database/database";
 
 /**
  * Generated class for the NewGamePage page.
@@ -18,7 +19,12 @@ export class NewGamePage {
 
     private myForm;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private fb: FormBuilder) {
+  constructor(
+      public navCtrl: NavController,
+      public navParams: NavParams,
+      private fb: FormBuilder,
+      private afDatabase: AngularFireDatabase
+  ) {
       this.myForm = this.fb.group({
           title: ['', Validators.required ],
           description: ['', Validators.required ],
@@ -29,7 +35,13 @@ export class NewGamePage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad NewGamePage');
   }
-    
-    
+
+    addGame() {
+        const games = this.afDatabase.list('games');
+
+        var data = this.myForm.value;
+        data.platforms = data.platforms.split(',');
+        games.push(this.myForm.value).then(result => this.navCtrl.pop())
+    }
 
 }
